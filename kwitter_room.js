9475070aa5bcdb4a55384a8bde9446563b7ab080
11hitesh <72382736+11hitesh@users.noreply.github.com>
2021-07-07 15:56:16 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,36 @@
+var firebaseConfig = { apiKey: "AIzaSyADh5Ni-uJL2PfS3Dn-GPEyfx2ZY1h2auE", authDomain: "project-94-6f52c.firebaseapp.com", projectId: "project-94-6f52c", storageBucket: "project-94-6f52c.appspot.com", messagingSenderId: "778849994640", appId: "1:778849994640:web:ee90e1fe8bb908b7a85515" }; firebase.initializeApp(firebaseConfig); function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key; Room_names = childKey; });});} getData(); function addRoom(){ room_name= document.getElementById("room_name").value; firebase.database().ref("/").child(room_name).update ({purpose:"adding room"}) }
+      // Initialize Firebase 
+    user_name=localStorage.getItem("user_name");
+
+document.getElementById("user_name").innerHTML="Welcome"+ user_name+"!";
+
+function addRoom() { 
+room_name = document.getElementById("room_name").value; 
+firebase.database().ref("/").child(room_name).update({ 
+purpose : "adding room name" });
+
+localStorage.setItem("room_name",room_name);
+window.location="kwitter_page.html";
+}
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+
+      //Start code
+console.log("Room Name - " + Room_names);
+row = "<div class='room_name' id="+Room_names+"onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+document.getElementById("output").innerHTML += row;
+      //End code
+      });});}
+getData();
+function redirectToRoomName(name) {
+      console.log(name);
+      localStorage.setItem("room_name",name);
+      window.location="kwitter_page.html";
+}
+
+function logout() {
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location = "index.html";
+}
